9d94c8a714ebeb2f4beeb7c5048cc26ba4206765
Filipe Nogueira <nog.lipe@gmail.com>
2025-03-19 16:58:03 -0300

    feat: login e home

---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/UI/card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/UI/label";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
+      <Card className="w-full max-w-md">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Entre com suas credenciais para acessar sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Usuário</Label>
+              <Input id="user" type="text" placeholder="usuario" required />
+            </div>
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <Label htmlFor="password">Senha</Label>
+                <a href="#" className="text-sm text-primary hover:underline">
+                  Esqueceu a senha?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col space-y-4">
+          <Button className="w-full">Entrar</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import Footer from "@/components/footer";
-import Menu from "@/components/Menu";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-full">
-      <Menu />
-      <div className="container">
-        <main className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold text-center">Bem-vindo à VipApp</h1>
-          <p className="text-lg text-center">Entre para acessar.</p>
-        </main>
-      </div>
-      <Footer />
-    </div>
+    <main className="flex flex-col h-screen items-center justify-center py-12 space-y-6">
+      <h1 className="text-4xl font-bold text-center">Bem-vindo à VipApp</h1>
+      <p className="text-lg text-center">Entre para acessar.</p>
+      <Button size="lg" className="mt-4" asChild>
+        <a href="/login">Entrar</a>
+      </Button>
+    </main>
   );
 }
