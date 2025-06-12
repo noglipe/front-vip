13c669ee2f52062cf3414deecd9c4e1d3ab5379e
Filipe Nogueira <nog.lipe@gmail.com>
2025-06-12 08:07:30 -0300

    feat: Grafico Caixas em Relatorio Anual

---
+++ src/app/app/transacoes/(relatorios)/anual/_components/graficosCaixas.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import * as React from "react";
+
+import { Label, Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/UI/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/UI/chart";
+import { formatReal } from "@/lib/utils";
+
+interface ExternalData {
+  nome: string;
+  saldoTotal: number;
+  total: {
+    totalReceita: number;
+    totalDespesa: number;
+    total: number;
+  };
+}
+
+interface GraficoCaixasProps {
+  data: ExternalData;
+}
+
+export default function GraficoCaixas({ data }: GraficoCaixasProps) {
+  const normalizeFactor = 10000;
+
+  const chartData = React.useMemo(() => {
+    return [
+      {
+        legenda: "Receita",
+        valor: Math.abs(data.total.totalReceita),
+        fill: "var(--chart-2)",
+      },
+      {
+        legenda: "Despesa",
+        valor: Math.abs(data.total.totalDespesa),
+        fill: "var(--chart-5)",
+      },
+    ];
+  }, [data]);
+
+  const totalVisitors = React.useMemo(() => {
+    return Math.abs(data.total.total); // usa total absoluto, para evitar números negativos no centro
+  }, [data]);
+
+  const chartConfig = {
+    Receita: {
+      label: "Receita",
+      color: "var(--chart-1)",
+    },
+    Despesa: {
+      label: "Despesa",
+      color: "var(--chart-2)",
+    },
+  } satisfies ChartConfig;
+
+  function CustomTooltipContent({ payload, label, active }: any) {
+    if (active && payload && payload.length) {
+      const data = payload[0];
+      const cor = data.fill || data.payload.fill;
+      return (
+        <div className="p-2 rounded border shadow bg-black">
+          <div
+            className="p-2 w-4 rounded border shadow"
+            style={{ backgroundColor: `${cor}` }}
+          ></div>
+          <p>{data.name}</p>
+          <p>
+            {formatReal(data.value)}{" "}
+            {/* multiplica pelo fator de normalização */}
+          </p>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>{data.nome}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<CustomTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="valor"
+              nameKey="legenda"
+              innerRadius={60}
+              strokeWidth={1}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-md font-bold"
+                        >
+                          {formatReal(totalVisitors)}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Saldo Filtro
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Saldo total: {formatReal(data.saldoTotal)}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/app/transacoes/(relatorios)/anual/page.tsx
+++ src/app/app/transacoes/(relatorios)/anual/page.tsx
@@ -14,14 +14,6 @@ import {
 import { DollarSign, TrendingDown, TrendingUp } from "lucide-react";
 import { formatReal } from "@/lib/utils";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/UI/accordion";
-import { ScrollArea } from "@/components/UI/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/UI/tabs";
-import {
   Table,
   TableBody,
   TableCell,
@@ -29,6 +21,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/UI/table";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/UI/chart";
+import GraficoCaixas from "./_components/graficosCaixas";
+import GraficoMeses from "./_components/graficoMeses";
 
 type Custos = {
   nome: string;
@@ -38,15 +38,33 @@ type Custos = {
   };
 };
 
+type Caixas = {
+  nome: string;
+  saldoTotal: number;
+  total: {
+    totalReceita: number;
+    totalDespesa: number;
+    total: number;
+  };
+};
+
+type Ano = {
+  saltoTotal: number;
+  totalReceita: number;
+  totalDespesa: number;
+  mes: any[];
+};
+
 interface DadosRelatorio {
   totalReceita: number;
   totalDespesa: number;
   total: number;
   custosPorCentro: Custos[];
-  custosPorCaixas: Custos[];
+  custosPorCaixas: Caixas[];
+  ano?: Ano[];
 }
 
-export default function page() {
+export default function Page() {
   const [ano, setAno] = useState<number | null>(2025);
   const [mes, setMes] = useState<string | null | undefined>(null);
   const [selctAtivo, SetSelectAtivo] = useState(true);
@@ -68,6 +86,40 @@ export default function page() {
     "Dezembro",
   ];
 
+  const cores = [
+    "var(--color-1)",
+    "var(--color-2)",
+    "var(--color-3)",
+    "var(--color-4)",
+    "var(--color-5)",
+  ];
+
+  const chartConfig = {
+    visitors: {
+      label: "Visitas",
+    },
+    1: {
+      label: "1",
+      color: "hsl(var(--chart-1))",
+    },
+    2: {
+      label: "2",
+      color: "hsl(var(--chart-2))",
+    },
+    3: {
+      label: "3",
+      color: "hsl(var(--chart-3))",
+    },
+    4: {
+      label: "4",
+      color: "hsl(var(--chart-4))",
+    },
+    5: {
+      label: "5",
+      color: "hsl(var(--chart-5))",
+    },
+  } satisfies ChartConfig;
+
   useEffect(() => {
     if (typeof ano === "number") {
       SetSelectAtivo(false);
@@ -77,7 +129,6 @@ export default function page() {
 
   async function consultaAno() {
     setLoading(true);
-
     try {
       let url = "";
       if (mes) {
@@ -86,15 +137,12 @@ export default function page() {
         url = `${process.env.NEXT_PUBLIC_BACKEND_URL}financeiro/relatorio/?ano=${ano}`;
       }
       const response = await fetch(url);
-
       if (!response.ok) {
-        console.log(response.status);
+        console.error(response.status);
+        return;
       }
-
       const data = await response.json();
-
       console.log(data);
-
       setDados(data);
     } catch (error) {
       console.error(error);
@@ -108,27 +156,29 @@ export default function page() {
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
           <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-            Relatório Financeito {mes && `- ${meses[parseInt(mes) - 1]} de`}{" "}
-            {ano}{" "}
+            Relatório Financeiro {mes && `- ${meses[parseInt(mes) - 1]} de`}{" "}
+            {ano}
           </h1>
           <p className="text-muted-foreground text-sm">
-            Visão geral das Financas
+            Visão geral das Finanças
           </p>
         </div>
-        <div className=" flex flex-row gap-2">
+        <div className="flex flex-row gap-2">
           <DialogAno ano={ano} setAno={setAno} />
           <SelectMes setMes={setMes} mes={mes} ativo={selctAtivo} />
         </div>
       </div>
+
       <div className="container mt-2">
         {loading ? (
           <Loading />
         ) : (
           dados && (
-            <div>
+            <>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {/* Card Entrada */}
                 <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardHeader className="flex flex-row items-center justify-between pb-2">
                     <CardTitle className="text-sm font-medium">
                       Total de Entradas
                     </CardTitle>
@@ -136,15 +186,17 @@ export default function page() {
                   </CardHeader>
                   <CardContent>
                     <div className="text-2xl font-bold text-green-600">
-                      {formatReal(dados?.totalReceita)}
+                      {formatReal(dados.totalReceita)}
                     </div>
                     <p className="text-xs text-muted-foreground">
                       Receitas do ano {ano}
                     </p>
                   </CardContent>
                 </Card>
+
+                {/* Card Saída */}
                 <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardHeader className="flex flex-row items-center justify-between pb-2">
                     <CardTitle className="text-sm font-medium">
                       Total de Saídas
                     </CardTitle>
@@ -152,7 +204,7 @@ export default function page() {
                   </CardHeader>
                   <CardContent>
                     <div className="text-2xl font-bold text-red-600">
-                      {formatReal(dados?.totalDespesa)}
+                      {formatReal(dados.totalDespesa)}
                     </div>
                     <p className="text-xs text-muted-foreground">
                       Despesas do ano {ano}
@@ -160,8 +212,9 @@ export default function page() {
                   </CardContent>
                 </Card>
 
+                {/* Card Saldo */}
                 <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardHeader className="flex flex-row items-center justify-between pb-2">
                     <CardTitle className="text-sm font-medium">
                       Saldo Anual
                     </CardTitle>
@@ -177,7 +230,7 @@ export default function page() {
                         dados.total >= 0 ? "text-green-600" : "text-red-600"
                       }`}
                     >
-                      {formatReal(dados?.total)}
+                      {formatReal(dados.total)}
                     </div>
                     <p className="text-xs text-muted-foreground">
                       {dados.total >= 0
@@ -188,54 +241,21 @@ export default function page() {
                 </Card>
               </div>
 
+              {/* Chart de Caixa */}
               <Card className="mt-4 shadow-md rounded-xl border border-gray-200">
                 <CardContent className="p-4">
                   <h2 className="text-xl font-semibold mb-4">Caixa</h2>
-
-                  <Accordion
-                    type="single"
-                    collapsible
-                    className="grid gap-4 md:grid-cols-4"
-                  >
-                    {dados.custosPorCaixas.map((v) => (
-                      <div 
-                        key={v.nome}
-                        className="border rounded-lg p-4 shadow-sm bg-muted/50 hover:bg-muted transition-all duration-200"
-                      >
-                        <div className="flex justify-between items-center">
-                          <h3 className="text-lg font-medium mb-2">{v.nome}</h3>
-
-                          <div className="text-sm">
-                            <p className="font-semibold">
-                              {formatReal(
-                                (v.total.totalReceita || 0) -
-                                  (v.total.totalDespesa || 0)
-                              )}
-                            </p>
-                          </div>
-                        </div>
-
-                        <div className="flex flex-col justify-between ">
-                          <div className="text-sm">
-                            <p className="text-muted-foreground">Receita</p>
-                            <p className="font-medium">
-                              {formatReal(v.total.totalReceita)}
-                            </p>
-                          </div>
-
-                          <div className="text-sm">
-                            <p className="text-muted-foreground">Despesa</p>
-                            <p className="font-medium">
-                              {formatReal(v.total.totalDespesa)}
-                            </p>
-                          </div>
-                        </div>
+                  <div className="grid gap-2 md:grid-cols-4">
+                    {dados.custosPorCaixas.map((caixa) => (
+                      <div>
+                        <GraficoCaixas data={caixa} />
                       </div>
                     ))}
-                  </Accordion>
+                  </div>
                 </CardContent>
               </Card>
 
+              {/* Tabela Centro de Custo */}
               <Card className="mt-2">
                 <CardHeader>
                   <CardTitle>Centro de Custo</CardTitle>
@@ -256,22 +276,22 @@ export default function page() {
                           <TableRow key={item.nome}>
                             <TableCell>{item.nome}</TableCell>
                             <TableCell
-                              className={`${
+                              className={
                                 item.total.totalReceita < 0
                                   ? "text-red-600"
                                   : "text-green-600"
-                              }`}
+                              }
                             >
                               {item.total.totalReceita
                                 ? formatReal(item.total.totalReceita)
                                 : "-"}
                             </TableCell>
                             <TableCell
-                              className={`${
+                              className={
                                 item.total.totalDespesa <= 0
                                   ? "text-red-600"
                                   : "text-green-600"
-                              }`}
+                              }
                             >
                               {item.total.totalDespesa
                                 ? formatReal(item.total.totalDespesa)
@@ -284,8 +304,13 @@ export default function page() {
                   </div>
                 </CardContent>
               </Card>
-              <div className="grid grid-cols-1 gap-6"></div>
-            </div>
+
+              <Card>
+                <CardContent>
+                  <GraficoMeses />
+                </CardContent>
+              </Card>
+            </>
           )
         )}
       </div>
