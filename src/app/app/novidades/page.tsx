d354b9d3dad371d9ce10518ea8ead1dde3f7fba5
Filipe Nogueira <nog.lipe@gmail.com>
2025-06-24 08:56:10 -0300

    Feat: Pagina Novidades

---
+++ src/app/app/novidades/page.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/UI/card";
+
+export default function page() {
+  return <Card className="">
+    <CardHeader>
+        <CardTitle>
+            <h1>Novidades e Lançamentos</h1>
+        </CardTitle>
+        <CardDescription>
+            Aqui você ficará sabendo das novidades e atualizações do sistema.
+        </CardDescription>
+        <CardContent>
+            
+        </CardContent>
+    </CardHeader>
+  </Card>;
+}
