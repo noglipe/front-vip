554e2a6711b1287c32e667321d52257c7e033d56
Filipe Nogueira <nog.lipe@gmail.com>
2025-07-02 09:07:40 -0300

    Feat: redirecionamento admim

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://vipapp-production.up.railway.app/admin",
+        permanent: false, // use true se quiser um redirecionamento 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
