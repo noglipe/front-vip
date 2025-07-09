6a9b4f087cbdf99b445e354cccb2a9935115e486
Filipe Nogueira <nog.lipe@gmail.com>
2025-07-09 09:44:50 -0300

    layoit: Tela Principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import { Button } from "../components/UI/button";
+import img from "../../public/logos/brancaFVP.png";
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen items-center justify-center py-12 space-y-6">
-      <h1 className="text-4xl font-bold text-center">Bem-vindo à VipApp</h1>
-      <p className="text-lg text-center">Entre para acessar.</p>
-      <Button size="lg" className="mt-4 bg-black text-white" asChild>
+    <main className="flex flex-col min-h-screen items-center justify-center py-12 space-y-6">
+      <div className="w-48 sm:w-auto h-auto">
+        <Image
+          src={img}
+          alt="Logo da Igreja Família Vida e Paz"
+          width={384} 
+          height={384}
+          priority
+        />
+      </div>
+
+      <Button size="lg" className="mt-4" asChild>
         <a href="/login">Entrar</a>
       </Button>
     </main>
